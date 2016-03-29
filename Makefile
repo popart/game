adf26a31550f29c0f23340c42a1e32ee0a055ca4
andrew wong <andrew.wong@findmine.us>
2016-03-29 13:22:21 -0400

    Draw a triangle

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
 #export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/include
 all:
-	gcc -o myprogram myprogram.c -lGLEW  `sdl2-config --cflags --libs`
+	gcc -o myprogram myprogram.c -lGLEW -lGL `sdl2-config --cflags --libs`
--- myprogram.c
+++ myprogram.c
@@ -4,8 +4,7 @@
 #include <SDL_opengl.h>
 #include <stdio.h>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     SDL_Init(SDL_INIT_EVERYTHING);
 
     // configure SDl to use openGL 3.2 context
@@ -27,7 +26,61 @@ int main(int argc, char *argv[])
     GLuint vertexBuffer;
     glGenBuffers(1, &vertexBuffer);
 
-    printf("%u\n", vertexBuffer);
+    fprintf(stderr, "OpenGL %s\n", glGetString(GL_VERSION));
+
+
+    // Vertex Array Object
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    // Vertex Buffer Object
+    GLuint vbo;
+    glGenBuffers(1, &vbo); // Generate 1 buffer
+    glBindBuffer(GL_ARRAY_BUFFER, vbo); //activate VBO
+
+    // some vertices
+    float vertices[] = {
+      0.0f,  0.5f, // Vertex 1 (X, Y)
+      0.5f, -0.5f, // Vertex 2 (X, Y)
+      -0.5f, -0.5f  // Vertex 3 (X, Y)
+    };
+
+    // copy vertices to VBO
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // initialize shaders
+    const char * vertexSource;
+    int size;
+    GLuint vertexShader;
+    size = ae_load_file_to_memory("vertex_shader.c", &vertexSource);
+    if(size > 0) {
+      vertexShader = glCreateShader(GL_VERTEX_SHADER);
+      glShaderSource(vertexShader, 1, &vertexSource, NULL);
+      glCompileShader(vertexShader);
+    }
+
+    const char * fragmentSource;
+    GLuint fragmentShader;
+    size = ae_load_file_to_memory("fragment_shader.c", &fragmentSource);
+    if(size > 0) {
+      fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+      glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+      glCompileShader(fragmentShader);
+    }
+
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    // not necessary now b/c only one output
+    glBindFragDataLocation(shaderProgram, 0, "outColor");
+    glLinkProgram(shaderProgram);
+    glUseProgram(shaderProgram);
+
+    // link vertex & data attributes
+    GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
+    glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(posAttrib);
 
     // main loop
     SDL_Event windowEvent;
@@ -39,10 +92,31 @@ int main(int argc, char *argv[])
         }
 
         SDL_GL_SwapWindow(window);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
     }
 
     // delete OpenGL context
     SDL_GL_DeleteContext(context);
     SDL_Quit();
     return 0;
+}
+
+int ae_load_file_to_memory(const char *filename, char **result) {
+  int size = 0;
+  FILE *f = fopen(filename, "rb");
+  if (f == NULL) {
+    *result = NULL;
+    return -1; // -1 means file opening fail
+  }
+  fseek(f, 0, SEEK_END);
+  size = ftell(f);
+  fseek(f, 0, SEEK_SET);
+  *result = (char *)malloc(size+1);
+  if (size != fread(*result, sizeof(char), size, f)) {
+    free(*result);
+    return -2; // -2 means file reading fail
+  }
+  fclose(f);
+  (*result)[size] = 0;
+  return size;
 }
